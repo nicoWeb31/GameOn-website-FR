b30ec78b3be0049a467dfed524e045c130100788
NicoRiot <nriot@umoove-inc.com>
2021-06-27 21:44:01 +0200

    recupération des elements, validations

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,9 +10,18 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const btnSubmit = document.querySelectorAll(".btn-submit");
-
+const btnSubmit = document.querySelector(".btn-submit");
+const inputName = document.querySelector("#last");
+const birthday = document.querySelector("#birthdate");
 const formData = document.querySelectorAll(".formData");
+const cuCheckBox = document.querySelectorAll("#checkbox1");
+
+const loc1 = document.querySelector("#location1");
+const loc2 = document.querySelector("#location2");
+const loc3 = document.querySelector("#location3");
+const loc4 = document.querySelector("#location4");
+const loc5 = document.querySelector("#location5");
+const loc6 = document.querySelector("#location6");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -24,6 +33,38 @@ function launchModal() {
 
 //comfirm ok button
 btnSubmit.addEventListener("click", () => {
-  alert("Merci ! Votre réservation a été reçue.");
-  console.log('resa ok ')
+  let error = "";
+  if (inputName.value.length < 2) {
+    error += "Veuillez entrer 2 caractères ou plus pour le champ du nom. \n";
+  }
+
+  if (!birthday.value) {
+    error += "Vous devez entrer votre date de naissance. \n";
+  }
+
+  if (!cuCheckBox[0].checked) {
+    error +=
+      "Vous devez vérifier que vous acceptez les termes et conditions. \n";
+  }
+
+  if (
+    !loc1.value ||
+    !loc2.value ||
+    !loc3.value ||
+    !loc4.value ||
+    !loc4.value ||
+    !loc5.value ||
+    !loc6.value
+  ) {
+    error += "Vous devez choisir une option.\n";
+  }
+
+  alert(error ? error : "résa ok ");
 });
+
+/* ---------------------------- formulaire valid ---------------------------- */
+if(true){
+  btnSubmit.disabled = false;
+}
+
+
